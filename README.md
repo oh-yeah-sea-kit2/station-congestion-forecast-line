b0f9a300b13186dd65d28543ca3a150de0adab5c
oh-yeah-sea-kit2 <spyroach.204@gmail.com>
2019-12-11 14:07:11 +0900

    add ユーザ情報を返す機能

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # station-congestion-forecast-line
+- https://dashboard.heroku.com/apps/station-congestion-forecast
+
+## herokuへdeply時
+- git add .
+- git commit -m "コメント"
+- git push heroku master
+
--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
+# from linebot.models import (
+#     FollowEvent, MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction, MessageTemplateAction, URITemplateAction
+# )
 from linebot.models import (
-    FollowEvent, MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction, MessageTemplateAction, URITemplateAction
+    MessageEvent, TextMessage, MessageAction, TemplateSendMessage, TextSendMessage, ButtonsTemplate
 )
 import os
 
@@ -44,10 +47,20 @@ def callback():
 # MessageEvent
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='「' + event.message.text + '」')
-     )
+    profile = line_bot_api.get_profile(event.source.user_id)
+    status_msg = profile.status_message
+    # LINEに登録されているstatus_messageが空の場合は、"なし"という文字列を代わりの値とする
+    if status_msg != "None":
+        status_msg = "なし"
+    # messages = TemplateSendMessage(alt_text="Buttons template",
+    #                                template=ButtonsTemplate(
+    #                                    thumbnail_image_url=profile.picture_url,
+    #                                    title=profile.display_name,
+    #                                    text=f"User Id: {profile.user_id[:5]}...\n"
+    #                                         f"Status Message: {status_msg}",
+    #                                    actions=[MessageAction(label="成功", text="次は何を実装しましょうか？")]))
+    messages = TextSendMessage(text='User Id:' + profile.user_id)
+    line_bot_api.reply_message(event.reply_token, messages=messages)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT"))
